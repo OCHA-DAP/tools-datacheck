b9639fda6ae249a57d12548c0962d43b4466cc51
Alexandru Artimon <aalecs@gmail.com>
2018-04-22 10:21:58 +0200

    base angular project

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  hxlProxy: 'https://proxy.hxlstandard.org/data.json',
+  hxlCheck: 'http://www.mocky.io/v2/59e0f52f0f00003405effd4c', // succesful hxl check
+  // hxlCheck: 'http://www.mocky.io/v2/59ec7fc03100009601d24e0e', // failing hxl check
+  hxlPreview: 'https://tools.humdata.org/quickcharts/app',
+  snapService: 'http://localhost:8080',
+  googleAnalyticsKey: 'UA-48221887-3',
+  prodMixpanelKey: '99035923ee0a67880e6c05ab92b6cbc0', // actually hdx-server project not prod
+  testMixpanelKey: '875bfe50f9cb981f4e2817832c83c165',
+  prodHostname: 'tools.humdata.org' // used by analytics code to decide which key to use
+};
